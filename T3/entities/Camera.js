55fd264b3203c3a5b8674f52e690cc1933b8b1f5
Gabriel Frasson <bielfc27@gmail.com>
2023-11-24 19:15:55 -0300

    Lower camera position

--- T3/entities/Camera.js
+++ T3/entities/Camera.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 
 export class Camera {
     constructor() {
-        this.initialPosition = new THREE.Vector3(0, 75, 56);
+        this.initialPosition = new THREE.Vector3(0, 50, 64);
 
         this.aspectRatio = window.innerWidth / window.innerHeight;
         this.near = 0.1;
@@ -11,7 +11,7 @@ export class Camera {
 
         this.camera = new THREE.PerspectiveCamera(this.fov, this.aspectRatio, this.near, this.far);
         this.camera.position.copy(this.initialPosition);
-        this.camera.lookAt(new THREE.Vector3(0, 0, 12));
+        this.camera.lookAt(new THREE.Vector3(0, 0, 19));
         
         this.camera.layers.enable(0);
     }
