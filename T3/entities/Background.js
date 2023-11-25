ac9d81cf56248d157c8328d6cb2b843be6f5e1ee
GabrielGomesBahia <gabrielgbahia@gmail.com>
2023-11-25 14:20:08 -0300

    ajustando skybox

--- T3/entities/Background.js
+++ T3/entities/Background.js
@@ -59,36 +59,4 @@ export class Background {
         let cubeMapTexture = new THREE.CubeTextureLoader().load(urls);
         scene.background = cubeMapTexture;
     }
-
-    // createSkyBox(scene) {
-    //     const materialArray = this.createMaterialArray("space");
-    //     //console.log("materialArray = ", materialArray)
-    //     this.skyboxGeo = new THREE.BoxGeometry(50, 100, 120);
-    //     //this.skyboxGeo = new THREE.BoxGeometry(120, 120, 120);
-    //     this.skybox = new THREE.Mesh(this.skyboxGeo, materialArray);
-    //     this.skybox.position.set(0, 50, 10)
-    //     scene.add(this.skybox);
-    // }
-
-    // createPathStrings(filename) {
-    //     const basePath = "./assets/skybox2/";
-    //     const baseFilename = basePath + filename;
-    //     const fileType = ".png";
-    //     const sides = ["ft", "bk", "up", "dn", "rt", "lf"];
-    //     const pathStings = sides.map(side => {
-    //         return baseFilename + "_" + side + fileType;
-    //     });
-    //     console.log("pathStings = ", pathStings)
-    //     return pathStings;
-    // }
-
-    // createMaterialArray(filename) {
-    //     const skyboxImagepaths = this.createPathStrings(filename);
-    //     const materialArray = skyboxImagepaths.map(image => {
-    //         let texture = new THREE.TextureLoader().load(image);
-    //         return new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
-    //     });
-    //     return materialArray;
-    // }
-
 }
