8ba0cdb3aa2ccceb20150419f05876f6023f7735
Gabriel Frasson <bielfc27@gmail.com>
2023-11-28 22:24:00 -0300

    Corrigindo aplicacao de textura no rebatedor

--- T3/entities/HitterWithDrag.js
+++ T3/entities/HitterWithDrag.js
@@ -1,41 +1,39 @@
-import { CSG } from '../../libs/other/CSGMesh.js'    
-import * as THREE from  'three'; 
-import {initRenderer, 
+import { CSG } from '../../libs/other/CSGMesh.js'
+import * as THREE from 'three';
+import {
+    initRenderer,
     setDefaultMaterial,
     getMaxSize
 } from "../../libs/util/util.js";
-import {DragControls} from '../../build/jsm/controls/DragControls.js'
-import {GLTFLoader} from '../../build/jsm/loaders/GLTFLoader.js';
+import { DragControls } from '../../build/jsm/controls/DragControls.js'
+import { GLTFLoader } from '../../build/jsm/loaders/GLTFLoader.js';
 
 export class HitterWithDrag {
     constructor(scene, camera, renderer) {
         this.auxMat = new THREE.Matrix4();
         this.material1 = setDefaultMaterial();
         this.cubeMesh = new THREE.Mesh(new THREE.BoxGeometry(4, 2, 16), this.material1)
-        this.cylinderMesh = new THREE.Mesh( new THREE.CylinderGeometry(8, 8, 2, 100))
+        this.cylinderMesh = new THREE.Mesh(new THREE.CylinderGeometry(8, 8, 2, 100))
         this.hitterMesh;
         this.csgObject;
         this.cubeCSG;
         this.cylinderCSG;
 
-        // cubeMesh.position.set(0, 0, 0)
-        //scene.add(cubeMesh)
         this.cylinderMesh.position.set(7, -0.1, 0.0)
         this.cylinderMesh.matrixAutoUpdate = false;
         this.cylinderMesh.updateMatrix();
-        //scene.add(cylinderMesh)
+
         this.cylinderCSG = CSG.fromMesh(this.cylinderMesh)
-        this.cubeCSG = CSG.fromMesh(this.cubeMesh)   
-        this.csgObject = this.cubeCSG.intersect(this.cylinderCSG) // Execute intersection
+        this.cubeCSG = CSG.fromMesh(this.cubeMesh)
+        this.csgObject = this.cubeCSG.intersect(this.cylinderCSG)
         this.hitterMesh = CSG.toMesh(this.csgObject, this.auxMat)
 
-        let texture = new THREE.TextureLoader().load('../../assets/textures/displacement/Stylized_blocks_001_roughness.jpg');
+        this.constructTexture(scene);
 
         this.hitterMesh.material = new THREE.MeshPhongMaterial({
             color: "#5FA1AD",
             shininess: "10",
-            specular: "rgb(255, 255, 255)",
-            map: texture
+            specular: "rgb(255, 255, 255)"
         });
 
         this.hitterMesh.position.set(0, 0, 2.0)
@@ -70,24 +68,78 @@ export class HitterWithDrag {
                 posX = -16.7;
             }
             self.move(posX);
-        });        
+        });
     }
 
-    loadGLBFile(asset, file, desiredScale, scene)
-    {
-        let loader = new GLTFLoader( );
+    constructTexture(scene) {
+        let v = [
+            -6.3, -2.0, 0.0, // 0
+            -5.5, -1.0, 0.0, // 1
+            -4.5, 0.2, 0.0,  // 2 
+            -3.0, 0.8, 0.0,  // 3 
+            -1.0, 1.5, 0.0, // 4 
+            0.0, -2.0, 0.0, // 5 MEIO
+            1.0, 1.5, 0.0, // 6 
+            3.0, 0.8, 0.0, // 7 
+            4.5, 0.2, 0.0, // 8 
+            5.5, -1.0, 0.0, // 9
+            6.3, -2.0, 0.0, // 10 
+        ];
+
+        let f = [
+            0, 1, 5,
+            1, 2, 5,
+            2, 3, 5,
+            3, 4, 5,
+            4, 6, 5,
+            6, 7, 5,
+            7, 8, 5,
+            8, 9, 5,
+            9, 10, 5,
+        ];
+
+        const n = v;
+        var vertices = new Float32Array(v);
+        var normals = new Float32Array(n);
+        var indices = new Uint32Array(f);
+        let geometry = new THREE.BufferGeometry();
+
+        geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+        geometry.setAttribute('normal', new THREE.BufferAttribute(normals, 3));
+        geometry.setIndex(new THREE.BufferAttribute(indices, 1));
+        geometry.computeVertexNormals();
+
+        let material = new THREE.MeshPhongMaterial({ color: "rgb(255,255,255)" });
+        material.side = THREE.DoubleSide;
+        material.flatShading = true;
+        this.meshTextureTop = new THREE.Mesh(geometry, material);
+        this.meshTextureTop.rotation.x = Math.PI / -2;
+        this.meshTextureTop.position.set(0, 2, 40.2)
+        this.meshTextureTop.material.opacity = 0.5
+        this.setTexture(this.meshTextureTop);
+
+        let geometry2 = new THREE.BoxGeometry(12.5, 2, 0);
+        this.meshTextureBack = new THREE.Mesh(geometry2, material);
+        this.meshTextureBack.position.set(0, 1, 42.2)
+
+        scene.add(this.meshTextureBack);
+        scene.add(this.meshTextureTop);
+    }
+
+    loadGLBFile(asset, file, desiredScale, scene) {
+        let loader = new GLTFLoader();
         let self = this;
-        loader.load( file, function ( gltf ) {
+        loader.load(file, function (gltf) {
             let obj = gltf.scene;
-            obj.traverse( function ( child ) {
-            if ( child.isMesh ) {
-                child.castShadow = true;
-                child.receiveShadow = true;
-            }
+            obj.traverse(function (child) {
+                if (child.isMesh) {
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                }
             });
             obj = self.normalizeAndRescaleAsset(obj, desiredScale);
             obj = self.fixPositionAsset(obj);
-            obj.updateMatrixWorld( true )
+            obj.updateMatrixWorld(true)
             scene.add(obj);
 
             asset.object = gltf.scene;
@@ -95,24 +147,18 @@ export class HitterWithDrag {
         }, null, null);
     }
 
-    normalizeAndRescaleAsset(obj, newScale)
-    {
+    normalizeAndRescaleAsset(obj, newScale) {
         var scale = getMaxSize(obj);
-        obj.scale.set(newScale * (1.0/scale),
-                        newScale * (1.0/scale),
-                        newScale * (1.0/scale));
+        obj.scale.set(newScale * (1.0 / scale),
+            newScale * (1.0 / scale),
+            newScale * (1.0 / scale));
         return obj;
     }
 
-    fixPositionAsset(obj)
-    {
-        var box = new THREE.Box3().setFromObject( obj );
+    fixPositionAsset(obj) {
+        var box = new THREE.Box3().setFromObject(obj);
         obj.rotation.y += Math.PI / 90;
-        obj.position.set(0, 0, 47)
-        if(box > 0)
-            obj.translateY(-box.min.y);
-        else
-            obj.translateY(-1*box.min.y);
+        obj.position.set(0, -8, 47)
         return obj;
     }
 
@@ -127,18 +173,30 @@ export class HitterWithDrag {
     move(pointX) {
         this.hitterMesh.position.set(pointX, 1, 40);
         this.sphere.position.set(pointX, 1, 47)
-        if(this.assetObj !== undefined) {
+        if (this.assetObj !== undefined) {
             this.assetObj.position.set(pointX, -8, 47)
+        }
+        if (this.meshTextureTop !== undefined) {
+            this.meshTextureTop.position.set(pointX, 2, 40.2);
+        }
+        if(this.meshTextureBack !== undefined) {
+            this.meshTextureBack.position.set(pointX, 1, 42.2)
         }
         this.updateBoundingBox();
     }
 
     resetPosition() {
-       this.hitterMesh.position.set(0, 1, 40)
-       this.sphere.position.set(0, 1, 46.5)
-       this.updateBoundingBox();
-       if(this.assetObj !== undefined) {
+        this.hitterMesh.position.set(0, 1, 40)
+        this.sphere.position.set(0, 1, 47)
+        this.updateBoundingBox();
+        if (this.assetObj !== undefined) {
             this.assetObj.position.set(0, -8, 47)
+        }
+        if (this.meshTextureTop !== undefined) {
+            this.meshTextureTop.position.set(0, 2, 40.2);
+        }
+        if(this.meshTextureBack !== undefined) {
+            this.meshTextureBack.position.set(0, 1, 42.2)
         }
     }
 
@@ -150,39 +208,24 @@ export class HitterWithDrag {
         let geometry = mesh.geometry;
         let material = mesh.material;
 
-        var positionBuffer = this.hitterMesh.geometry.attributes.position;
-        // Obtenha os arrays de posição x, y e z
-        var positionsX = positionBuffer.array.slice(0, positionBuffer.count);
-        var positionsY = positionBuffer.array.slice(positionBuffer.count, 2 * positionBuffer.count);
-        var positionsZ = positionBuffer.array.slice(2 * positionBuffer.count, 3 * positionBuffer.count);
+        var uvCoords = [
+            0.0, 0.0,   // 0
+            0.2, 0.5,   // 1
+            0.4, 0.0,   // 2
+            0.6, 0.5,   // 3
+            0.8, 0.0,   // 4
+            1.0, 0.5,   // 5 (MEIO)
+            0.8, 0.0,   // 6
+            0.6, 0.5,   // 7
+            0.4, 0.0,   // 8
+            0.2, 0.5,
+            0.0, 0.0];
 
-      
-        // You must set an individual UV coordinate for each vertex of your scene
-        // Learn more here:
-        // https://discoverthreejs.com/book/first-steps/textures-intro/
-        var uvCoords = [];
-        var j = 0;
-        for(let i = 0; i < positionsX.length; i++) {
-            uvCoords[j] = positionsX[i];
-            j = j+2;
-        }
+        geometry.setAttribute('uv', new THREE.BufferAttribute(new Float32Array(uvCoords), 2));
 
-        var j = 1;
-        for(let i = 0; i < positionsY.length; i++) {
-            uvCoords[j] = positionsX[i];
-            j = j+2;
-        }
-
-        // var uvCoords = [2, -0.4,
-        //     0.8999999761581421, 0.8999999761581421,
-        //     -6.241202354431152, -2.9449963569641113
-        //                 ];
-      
-        geometry.setAttribute( 'uv', new THREE.BufferAttribute( new Float32Array( uvCoords), 2 ) );
-      
-        // Load the texture and set to the material of the mesh
         let texture = new THREE.TextureLoader().load('../../assets/textures/displacement/Stylized_blocks_001_roughness.jpg');
-        material.map =  texture;
-      }
-      
+        material.map = texture;
+    }
+
+
 }
